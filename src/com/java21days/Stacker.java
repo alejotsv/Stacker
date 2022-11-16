11cb620e4477e3b1ff0e4431679318cf73275c26
Alejandro Salas <asalas.wd@outlook.com>
2022-11-15 21:05:34 -0500

    Initial commit

---
+++ src/com/java21days/Stacker.java
@@ -0,0 +1,17 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Stacker extends JFrame {
+    Dimension dim = new Dimension(300,300);
+
+    Stacker(String title){
+        super(title);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(dim);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+}
