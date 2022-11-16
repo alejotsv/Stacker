3afbc0a108504241f8a826e5e4fc59a6a1690412
Alejandro Salas <asalas.wd@outlook.com>
2022-11-15 21:12:25 -0500

    Add method to create buttons

--- src/com/java21days/Main.java
+++ src/com/java21days/Main.java
@@ -2,6 +2,7 @@ package com.java21days;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My main method");
+        String[] buttons = { "Subscribe", "Unsuscribe", "Refresh", "Save" };
+        Stacker stacker = new Stacker("Stacker", buttons);
     }
 }
--- src/com/java21days/Stacker.java
+++ src/com/java21days/Stacker.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Stacker extends JFrame {
     Dimension dim = new Dimension(300,300);
 
-    Stacker(String title){
+    Stacker(String title, String[] buttons){
         super(title);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(dim);
@@ -15,5 +15,15 @@ public class Stacker extends JFrame {
         setVisible(true);
     }
 
+    JButton[] createButtons(String[] buttons){
+        JButton[] jButtons = new JButton[buttons.length];
+        int i = 0;
+        for( String text : buttons ){
+            JButton btn = new JButton(text);
+            jButtons[i] = btn;
+            i++;
+        }
+        return jButtons;
+    }
 
 }
